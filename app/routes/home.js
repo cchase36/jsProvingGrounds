cd4f6cfbc1079a236f8b56f806c95e6a9c4554fa
Cory Chase <cchase@Corys-MBP.fios-router.home>
2015-08-09 23:29:40 -0400

    fixing title

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', 'JSProvingGrounds');
+  res.render('home', {title: 'JSProvingGrounds'});
 });
 
 module.exports = router;
