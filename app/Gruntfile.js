9a0c9b161304531b121a11afe081d02aceea4d89
Cory Chase <cchase@Corys-MBP.fios-router.home>
2015-08-09 23:03:26 -0400

    set up nav bar and added first blog post :)

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -1,60 +1,68 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    jsDir: 'public/javascripts/',
-    jsDistDir: 'dist/javascripts/',    
-    cssDir: 'public/stylesheets/',
-    cssDistDir: 'dist/stylesheets/',
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      js: {
-        options: {
-          separator: ';'
+    grunt.initConfig({
+        homeJsDir: 'public/javascripts/home',
+        jsDistDir: 'dist/javascripts/',
+        cssDir: 'public/stylesheets/',
+        cssDistDir: 'dist/stylesheets/',
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            home: {
+                options: {
+                    separator: ';' + grunt.util.linefeed,
+                    preserveComments : 'all'
+                },
+                src: ['node_modules/requirejs/require.js',
+                    '<%=homeJsDir%>*.js',
+                    'node_modules/jquery/dist/jquery.js',
+                    'node_modules/underscore/underscore-min.js',
+                    'node_modules/backbone/backbone.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.js'],
+                dest: '<%=jsDistDir%>home.js'
+            },
+            css: {
+                src: ['<%=cssDir%>*.css'/*,
+                    'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+                    'node_modules/bootstrap/dist/css/bootstrap.min.css'*/],
+                dest: '<%=cssDistDir%><%= pkg.name %>.css'
+            }
         },
-        src: ['<%=jsDir%>*.js'],
-        dest: '<%=jsDistDir%><%= pkg.name %>.js'
-      },
-      css: {
-        src: ['<%=cssDir%>*.css'],
-        dest: '<%=cssDistDir%><%= pkg.name %>.css'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          '<%=jsDistDir%><%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
-        }
-      }
-    },
-    cssmin: {
-      add_banner: {
-        options: {
-          banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    '<%=jsDistDir%>home.min.js': ['<%= concat.home.dest %>']
+                }
+            }
         },
-        files: {
-          '<%=cssDistDir%><%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
+        cssmin: {
+            add_banner: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%=grunt.template.today("dd-mm-yyyy") %> */\n'
+                },
+                files: {
+                    '<%=cssDistDir%><%= pkg.name %>.min.css': ['<%= concat.css.dest %>']
+                }
+            }
+        },
+        watch: {
+            files: ['<%=jsDir%>*.js', '<%=cssDir%>*.css'],
+            tasks: ['concat', 'uglify', 'cssmin']
         }
-      }
-    },
-    watch: {
-    files: ['<%=jsDir%>*.js', '<%=cssDir%>*.css'],
-    tasks: ['concat', 'uglify', 'cssmin']
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', [
-    'concat',
-    'uglify',
-    'cssmin',
-    'watch'
-  ]);
-  
+    grunt.registerTask('default', [
+        'concat',
+        'uglify',
+        'cssmin',
+        'watch'
+    ]);
+
 };
---
+++ app/public/javascripts/home/Router.js
@@ -0,0 +1,12 @@
+var HomeRouter = Backbone.Router.extend({
+    'routes' : {
+        '' : 'home',
+        'about' : 'about'
+    },
+    'home' : function() {
+        
+    },
+    'about' : function() {
+
+    }
+});
